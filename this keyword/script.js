a013f10b7f678382e70d647d3bd6156cc3b8f745
Aneel Grewal <aneel.grewal@hotmail.com>
2024-01-01 21:32:47 +0100

    this inside arrow function in object

--- this keyword/script.js
+++ this keyword/script.js
@@ -25,3 +25,35 @@ function print() {
 }
 window.print();
  */
+
+// this inside a object's method
+
+const obj = {
+  a: 10,
+  x: function () {
+    console.log(this); //obj object
+  },
+};
+
+obj.x();
+
+const obj1 = {
+  a: 10,
+  x: () => {
+    console.log(this); //window object because of enclosing lexical scope
+  },
+};
+
+obj1.x();
+
+const obj2 = {
+  a: 10,
+  x: function () {
+    y = () => {
+      console.log(this); //enclosing lexical scope i.e obj1 object
+    };
+    y();
+  },
+};
+
+obj2.x();
